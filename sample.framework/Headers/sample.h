3d301db96c27cb7d0964aaa406bfdbd1089ade50
Satheesh503649 <41050275+Satheesh503649@users.noreply.github.com>
2018-07-10 14:46:20 +0530

    Add files via upload

---
+++ sample.framework/Headers/sample.h
@@ -0,0 +1,19 @@
+//
+//  sample.h
+//  sample
+//
+//  Created by Satheesh Kumar on 10/07/18.
+//  Copyright © 2018 Satheesh Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for sample.
+FOUNDATION_EXPORT double sampleVersionNumber;
+
+//! Project version string for sample.
+FOUNDATION_EXPORT const unsigned char sampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sample/PublicHeader.h>
+
+
